9acc658d209fbd91cb92173224660f550146f268
Haseebm01 <haseebdr01@gmail.com>
2023-08-28 13:31:58 -0400

    modified

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -3,12 +3,12 @@ import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 import CanvasLoader from '../Loader';
 
-const Computers = () => { 
-  const computer = useGLTF('./desktop_pc/scene.gltf')
+const Computers = ({ isMobile }) => { 
+  const computer = useGLTF('./desktop_pc/scene.gltf');
   
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black" />
+      <hemisphereLight intensity={8} groundColor='black' />
       <pointLight intensity={1} />
       <spotLight
       position={[-20, 50, 10]}
@@ -20,8 +20,8 @@ const Computers = () => {
       />
       <primitive
       object={computer.scene}
-      scale={0.75}
-      position={[0, -3.25, -1.5]}
+      scale={isMobile ? 0.7 : 0.75}
+      position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
       rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -29,6 +29,23 @@ const Computers = () => {
 }
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuary = window.matchMedia('(max-width:500px)');
+
+    setIsMobile(mediaQuary.matches);
+
+    const handleMediaQuaryChange = (event) => {
+      setIsMobile(event.matches);
+    }
+    mediaQuary.addEventListener('change', handleMediaQuaryChange);
+
+    return () => {
+      mediaQuary.removeEventListener('change', handleMediaQuaryChange);
+    }
+  }, []);
+
   return (
     <Canvas
     frameloop="demand"
@@ -42,7 +59,7 @@ const ComputersCanvas = () => {
         maxPolarAngle={Math.PI / 2}
         minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
       <Preload all />
     </Canvas>
