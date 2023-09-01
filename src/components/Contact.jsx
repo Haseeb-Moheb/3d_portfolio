bc2c55be2eff753ba1e9f582c38d536d7e5a2251
Haseebm01 <haseebdr01@gmail.com>
2023-08-31 20:47:48 -0400

    modified

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -15,9 +15,49 @@ const Contact = () => {
   });
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => {}
+  const handleChange = (e) => {
+    const { name, value } =e.target;
 
-  const handleSubmit = (e) => {}
+    setForm({...form, [name]: value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    // rm0e8RcDptTsyOBI4
+    // template_3o6qksx
+    // service_y6jngoq
+
+    emailjs.send(
+      'service_y6jngoq',
+      'template_3o6qksx',
+      {
+        from_name: form.name,
+        to_name: 'Haseeb',
+        from_email: form.email,
+        to_email: 'haseebdr01@gmail.com',
+        message: form.message,
+      },
+      'rm0e8RcDptTsyOBI4'
+    )
+    .then(() => {
+      setLoading(false);
+      alert('Thank you! I will get back to you as soon as possible.');
+
+      setForm({
+        name: '',
+        email: '',
+        message: '',
+      })
+    }, (error) => {
+      setLoading(false)
+
+      console.log(error);
+
+      alert('Somthing went wrong.')
+    })
+  }
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import { ComputersCanvas } from './canvas';
 
 const Hero = () => {
   return (
-    <section className="relative w-full h-screen mx-auto">
+    <section className={`relative w-full h-screen mx-auto`}>
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7x1 mx-auto flex flex-row items-start gap-5`}>
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
@@ -38,6 +38,7 @@ const Hero = () => {
         </a>
       </div>
     </section> 
-  )
-}
+  );
+};
+
 export default Hero
