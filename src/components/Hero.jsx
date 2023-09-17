6b5e80aa9516f3c244b987487934a783c1606449
Haseebm01 <haseebdr01@gmail.com>
2023-09-17 14:46:18 -0400

    modified

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,21 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { motion } from 'framer-motion'
 import { styles } from '../styles'
 import { ComputersCanvas } from './canvas'
 import { SectionWrapper } from '../hoc'
+import { useInView } from 'react-intersection-observer'
+import { Preload } from '@react-three/drei'
 
 const Hero = () => {
+  const [ref, inView] = useInView();
+  // const [loaded, setLoaded] useState(false);
+
   return (
+    
     <section className={`relative w-full h-screen mx-auto` }>
 
-      <div
-        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}>
+      <div ref={ref}
+        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}> {inView && (
 
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
+
         </div>
+        )}
         <div>
+          
           <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#915eff]">Haseeb</span></h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop 3D visuals, user <br className="sm:block hidden" />
@@ -41,6 +50,7 @@ const Hero = () => {
             />
           </div>
         </a>
+        
       </div>
     </section> 
   );
--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -4,7 +4,7 @@ import { Points, PointMaterial, Preload  } from "@react-three/drei";
 import * as random from "maath/random/dist/maath-random.esm";
 
 const Stars = (props) => {
-  const ref =useRef();
+  const ref = useRef();
 
   const [sphere] = useState(()  => random.inSphere(new Float32Array(5000), { radius: 1.2 }));
 
